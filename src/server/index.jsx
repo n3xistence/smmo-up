6b04e82fe47c569d0ff768c238dc30a6e3773021
sguo1123 <62405530+sguo1123@users.noreply.github.com>
2023-01-13 21:09:49 -0500

    Backend Update - Weekly/Monthly Changes (Avg|Total|Diff)

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -24,7 +24,21 @@ app.get("/api/user/:id", (req, res) => {
             quests: [],
             levels: [],
             npc_kills: [],
-            user_kills: []
+            user_kills: [],
+            weekly_change: {
+                steps: [],
+                quests: [],
+                levels: [],
+                npc_kills: [],
+                user_kills: []
+            },
+            monthly_change: {
+                steps: [],
+                quests: [],
+                levels: [],
+                npc_kills: [],
+                user_kills: []
+            }
         }
 
         // grab sequential data
@@ -36,6 +50,96 @@ app.get("/api/user/:id", (req, res) => {
             dataset.user_kills.push(result[i].user_kills)
             dataset.levels.push(result[i].level)
         }
+
+        // grab historical data differences
+        if (result.length > 14) {
+            dataset.weekly_change.steps.push((result[result.length-1].steps - result[result.length-8].steps)/7)
+            dataset.weekly_change.steps.push(result[result.length-1].steps - result[result.length-8].steps)
+            dataset.weekly_change.steps.push(((result[result.length-1].steps - result[result.length-8].steps)-(result[result.length-8].steps - result[result.length-15].steps)))
+
+            dataset.weekly_change.quests.push((result[result.length-1].quests_performed - result[result.length-8].quests_performed)/7)
+            dataset.weekly_change.quests.push(result[result.length-1].quests_performed - result[result.length-8].quests_performed)
+            dataset.weekly_change.quests.push(((result[result.length-1].quests_performed - result[result.length-8].quests_performed)-(result[result.length-8].quests_performed - result[result.length-15].quests_performed)))
+
+            dataset.weekly_change.levels.push((result[result.length-1].level - result[result.length-8].level)/7)
+            dataset.weekly_change.levels.push(result[result.length-1].level - result[result.length-8].level)
+            dataset.weekly_change.levels.push(((result[result.length-1].level - result[result.length-8].level)-(result[result.length-8].level - result[result.length-15].level)))
+
+            dataset.weekly_change.npc_kills.push((result[result.length-1].npc_kills - result[result.length-8].npc_kills)/7)
+            dataset.weekly_change.npc_kills.push(result[result.length-1].npc_kills - result[result.length-8].npc_kills)
+            dataset.weekly_change.npc_kills.push(((result[result.length-1].npc_kills - result[result.length-8].npc_kills)-(result[result.length-8].npc_kills - result[result.length-15].npc_kills)))
+
+            dataset.weekly_change.user_kills.push((result[result.length-1].user_kills - result[result.length-8].user_kills)/7)
+            dataset.weekly_change.user_kills.push(result[result.length-1].user_kills - result[result.length-8].user_kills)
+            dataset.weekly_change.user_kills.push(((result[result.length-1].user_kills - result[result.length-8].user_kills)-(result[result.length-8].user_kills - result[result.length-15].user_kills)))
+
+        } else {
+
+            dataset.weekly_change.steps.push(-1)
+            dataset.weekly_change.steps.push(-1)
+            dataset.weekly_change.steps.push(0)
+
+            dataset.weekly_change.quests.push(-1)
+            dataset.weekly_change.quests.push(-1)
+            dataset.weekly_change.quests.push(0)
+
+            dataset.weekly_change.levels.push(-1)
+            dataset.weekly_change.levels.push(-1)
+            dataset.weekly_change.levels.push(0)
+
+            dataset.weekly_change.npc_kills.push(-1)
+            dataset.weekly_change.npc_kills.push(-1)
+            dataset.weekly_change.npc_kills.push(0)
+
+            dataset.weekly_change.user_kills.push(-1)
+            dataset.weekly_change.user_kills.push(-1)
+            dataset.weekly_change.user_kills.push(0)
+
+        }
+        
+        if (result.length > 60) {
+            dataset.monthly_change.steps.push((result[result.length-1].steps - result[result.length-31].steps)/30)
+            dataset.monthly_change.steps.push(result[result.length-1].steps - result[result.length-31].steps)
+            dataset.monthly_change.steps.push(((result[result.length-1].steps - result[result.length-31].steps)-(result[result.length-31].steps - result[result.length-61].steps)))
+
+            dataset.monthly_change.quests.push((result[result.length-1].quests_performed - result[result.length-31].quests_performed)/30)
+            dataset.monthly_change.quests.push(result[result.length-1].quests_performed - result[result.length-31].quests_performed)
+            dataset.monthly_change.quests.push(((result[result.length-1].quests_performed - result[result.length-31].quests_performed)-(result[result.length-31].quests_performed - result[result.length-61].quests_performed)))
+
+            dataset.monthly_change.levels.push((result[result.length-1].level - result[result.length-31].level)/30)
+            dataset.monthly_change.levels.push(result[result.length-1].level - result[result.length-31].level)
+            dataset.monthly_change.levels.push(((result[result.length-1].level - result[result.length-31].level)-(result[result.length-31].level - result[result.length-61].level)))
+
+            dataset.monthly_change.npc_kills.push((result[result.length-1].npc_kills - result[result.length-31].npc_kills)/30)
+            dataset.monthly_change.npc_kills.push(result[result.length-1].npc_kills - result[result.length-31].npc_kills)
+            dataset.monthly_change.npc_kills.push(((result[result.length-1].npc_kills - result[result.length-31].npc_kills)-(result[result.length-31].npc_kills - result[result.length-61].npc_kills)))
+
+            dataset.monthly_change.user_kills.push((result[result.length-1].user_kills - result[result.length-31].user_kills)/30)
+            dataset.monthly_change.user_kills.push(result[result.length-1].user_kills - result[result.length-31].user_kills)
+            dataset.monthly_change.user_kills.push(((result[result.length-1].user_kills - result[result.length-31].user_kills)-(result[result.length-31].user_kills - result[result.length-61].user_kills)))
+
+        } else {
+            dataset.monthly_change.steps.push(-1)
+            dataset.monthly_change.steps.push(-1)
+            dataset.monthly_change.steps.push(0)
+
+            dataset.monthly_change.quests.push(-1)
+            dataset.monthly_change.quests.push(-1)
+            dataset.monthly_change.quests.push(0)
+
+            dataset.monthly_change.levels.push(-1)
+            dataset.monthly_change.levels.push(-1)
+            dataset.monthly_change.levels.push(0)
+
+            dataset.monthly_change.npc_kills.push(-1)
+            dataset.monthly_change.npc_kills.push(-1)
+            dataset.monthly_change.npc_kills.push(0)
+
+            dataset.monthly_change.user_kills.push(-1)
+            dataset.monthly_change.user_kills.push(-1)
+            dataset.monthly_change.user_kills.push(0)
+        }
+
         return res.send(dataset)
     });   
 
